aa6a3ed00f0b795f667317290d097086068b842c
Chí Nguyễn Đắc <dacchi6761@gmail.com>
2021-06-05 08:24:46 +0700

    add react

--- src/main.js
+++ src/main.js
@@ -1,1 +1,8 @@
-console.log('Hello world 1234');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('app'),
+)
